6e7d2a437ea8c925740ed2a6e4a47173ece8d2fd
Bomzhchel <pzanadv@gmail.com>
2025-12-07 14:30:45 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Токен бота (ЗАМЕНИТЕ НА СВОЙ!)
-TOKEN = "8560106777:AAGKKuCJ6RUNZcqm10CQ2dLPBjSe1LoiTRI"
+TOKEN = "BOT_TOKEN"
 
 # Этапы разговора
 QUESTION, ANSWER = range(2)
